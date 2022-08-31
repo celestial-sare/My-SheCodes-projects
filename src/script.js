e5782a3b5619198c120364f989223456cb814fd0
Sare <sara.elizabethian@gmail.com>
2022-08-31 15:31:49 -0500

    Script update
    Added new search, submit, and city function

--- src/script.js
+++ src/script.js
@@ -30,12 +30,41 @@ function displayTemp(response) {
   );
 }
 
-let apiKey = "db75c95658bbbd4df7ff00c9187ad7d2";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Dallas&appid=${apiKey}&units=metric`;
+function search(city) {
+  let apiKey = "db75c95658bbbd4df7ff00c9187ad7d2";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemp);
+}
 
-axios.get(apiUrl).then(displayTemp);
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
 
-let now = new Date();
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
+
+function searchLocation(position) {
+  let apiKey = "a0debc08940b6553abc1991b0a844914";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemp);
+}
+
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+  let locationButton = document.querySelector("#location-button");
+  locationButton.addEventListener("click", getCurrentLocation);
+}
+
+function displayWeather(response) {
+  document.querySelector("#city-name").innerHTML = response.data.name;
+  document.querySelector("#temperature-head").innerHTML = Math.round(
+    response.data.main.temp
+  );
+}
+
 let days = [
   "Sunday",
   "Monday",
@@ -73,47 +102,3 @@ currentInfo.innerHTML = `${currentDay}, ${currentHour}:${currentMinutes}`;
 
 let fullDate = document.querySelector("#todays-date");
 fullDate.innerHTML = `${currentMonth} ${currentDate}, ${currentYear}`;
-
-function search(event) {
-  event.preventDefault();
-  let city = document.querySelector("#city-input").value;
-  searchCity(city);
-}
-
-let form = document.querySelector("#city-form");
-addEventListener("submit", search);
-
-function displayCurrentWeather(response) {
-  document.querySelector("#city-name").innerHTML = response.data.name;
-  document.querySelector("#temperature-head").innerHTML = `${Math.round(
-    response.data.main.temp
-  )}° C`;
-}
-function searchCity(city) {
-  let apiKey = "db75c95658bbbd4df7ff00c9187ad7d2";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(displayCurrentWeather);
-}
-
-function handleSubmit(event) {
-  event.preventDefault();
-  let city = document.querySelector("#city-input").value;
-  searchCity(city);
-}
-
-function searchLocation(position) {
-  let apiKey = "a0debc08940b6553abc1991b0a844914";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(displayCurrentWeather);
-}
-
-function getCurrentLocation(event) {
-  event.preventDefault();
-  navigator.geolocation.getCurrentPosition(searchLocation);
-}
-let locationButton = document.querySelector("#location-button");
-locationButton.addEventListener("click", getCurrentLocation);
-
-let searchForm = document.querySelector("#city-input");
-searchForm.addEventListener("submit", handleSubmit);
-let descriptionElement = document.querySelector("#description");
